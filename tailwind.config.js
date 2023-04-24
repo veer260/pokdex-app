a4c4eb22a9c5e8281f5492e2983f8fcbc73e90c6
veer260 <57021124+veer260@users.noreply.github.com>
2023-04-24 23:57:44 +0530

    added raleway font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      bit: ["Press Start 2P"],
+      raleway: ["Raleway"],
+    },
     extend: {},
   },
   plugins: [],
