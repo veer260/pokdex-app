5e20aef0711d25808176223eabcf6ce26a1877b3
veer260 <57021124+veer260@users.noreply.github.com>
2023-04-30 08:25:24 +0530

    added accent-color feature

--- src/pages/Pokemon.tsx
+++ src/pages/Pokemon.tsx
@@ -1,10 +1,60 @@
 //@ts-nocheck
-import React from "react";
+import React, { useEffect } from "react";
 import Wrapper from "../sections/WrapperHOC";
 import { Props } from "./Search";
+import { useAppSelector } from "../app/hooks";
+import { PokemonRoute, pokemonTabs } from "../utils/Constants";
+import Description from "./Pokemon/Description";
+import Evolution from "./Pokemon/Evolution";
+import Locations from "./Pokemon/Locations";
+import CapableMoves from "./Pokemon/CapableMoves";
+import { useParams } from "react-router";
+import { useSearchParams } from "react-router-dom";
+import { images, defaultImages } from "../utils/getPokemonImages";
+import extractColors from "extract-colors";
 
 const Pokemon: React.FC<Props> = ({ styling }) => {
-  return <div className={styling}>Pokemons</div>;
+  const params = useParams();
+
+  useEffect(() => {
+    const imageElemet = document.createElement("img");
+    imageElemet.src = images[params.id];
+    console.log("img:", imageElemet.src);
+    const options = {
+      pixels: 10000,
+      distance: 1,
+      splitPower: 10,
+      colorValidator: (red, green, blue, alpha = 255) => alpha > 250,
+      saturationDistance: 0.2,
+      lightnessDistance: 0.2,
+      hueDistance: 0.083333333,
+    };
+    const getColor = async () => {
+      console.log("helloe");
+      const color = await extractColors(imageElemet.src, options);
+      const root = document.documentElement;
+      console.log("style:", root.style);
+      root.style.setProperty("--accent-color", color[0].hex.split('"')[0]);
+    };
+    getColor();
+    let image = images[params.id];
+    if (!image) {
+      image = defaultImages[params.id];
+    }
+
+    // getPokemonInfo(image);
+  }, []);
+  const { currentPokemonTab } = useAppSelector((state) => state.app);
+  return (
+    <div className={styling}>
+      <div>
+        {currentPokemonTab == pokemonTabs.description && <Description />}
+        {currentPokemonTab == pokemonTabs.evolution && <Evolution />}
+        {currentPokemonTab == pokemonTabs.locations && <Locations />}
+        {currentPokemonTab == pokemonTabs.moves && <CapableMoves />}
+      </div>
+    </div>
+  );
 };
 
 export default Wrapper(Pokemon);
