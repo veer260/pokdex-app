b76c7b6dc67bf93a5ac237394578613450e8f422
veer260 <57021124+veer260@users.noreply.github.com>
2023-05-06 11:16:26 +0530

    added pokemon evolution page

--- src/pages/Pokemon/Evolution.tsx
+++ src/pages/Pokemon/Evolution.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { GenericPokemonType } from "../../utils/type";
+import getPokemonsData from "../../app/reducers/getPokemonsData";
+import { FaLessThan } from "react-icons/fa";
+import PokemonCards from "../../components/PokemonCards";
 
 const Evolution = () => {
-  return <div>Evolution</div>;
+  const dispatch = useAppDispatch();
+  const { currentPokemon, randomPokemons } = useAppSelector(
+    (state) => state.pokemon
+  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      //@ts-ignore
+      const pokemons: GenericPokemonType[] = currentPokemon?.evolution.map(
+        ({ pokemon }) => pokemon
+      );
+      await dispatch(getPokemonsData(pokemons));
+      setIsLoading(false);
+    };
+    fetchPokemon();
+  }, [dispatch, currentPokemon]);
+
+  if (isLoading) {
+    return <div className="text-white">Loading ...</div>;
+  }
+  return (
+    <div>
+      <PokemonCards randomPokemons={randomPokemons!} />
+    </div>
+  );
 };
 
 export default Evolution;
